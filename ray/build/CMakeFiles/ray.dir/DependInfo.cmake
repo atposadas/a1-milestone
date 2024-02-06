b31cb54445bacc27031b9d012f1440436358a8d8
Aaron Posadas <atpix@cs.utexas.edu>
2024-02-06 12:59:29 -0600

    adding files

---
+++ ray/build/CMakeFiles/ray.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/RayTracer.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/RayTracer.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/SceneObjects/Box.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/SceneObjects/Box.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/SceneObjects/Cone.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/SceneObjects/Cone.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/SceneObjects/Cylinder.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/SceneObjects/Cylinder.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/SceneObjects/Sphere.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/SceneObjects/Sphere.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/SceneObjects/Square.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/SceneObjects/Square.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/SceneObjects/trimesh.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/SceneObjects/trimesh.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/fileio/bitmap.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/fileio/bitmap.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/fileio/buffer.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/fileio/buffer.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/fileio/images.cc" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/fileio/images.cc.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/fileio/pngimage.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/fileio/pngimage.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/main.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/main.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/parser/JsonParser.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/parser/JsonParser.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/parser/Parser.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/parser/Parser.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/parser/ParserException.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/parser/ParserException.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/parser/Token.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/parser/Token.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/parser/Tokenizer.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/parser/Tokenizer.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/scene/bbox.cc" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/scene/bbox.cc.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/scene/camera.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/scene/camera.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/scene/cubeMap.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/scene/cubeMap.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/scene/light.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/scene/light.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/scene/material.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/scene/material.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/scene/ray.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/scene/ray.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/scene/scene.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/scene/scene.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/CommandLineUI.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/CommandLineUI.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/CubeMapChooser.cxx" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/CubeMapChooser.cxx.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/GraphicalUI.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/GraphicalUI.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/ModelerCamera.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/ModelerCamera.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/TraceGLWindow.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/TraceGLWindow.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/TraceUI.cc" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/TraceUI.cc.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/debuggingView.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/debuggingView.cpp.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/debuggingWindow.cxx" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/debuggingWindow.cxx.o"
+  "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/ui/glObjects.cpp" "/u/atpix/Documents/computer graphics/graphics - assignment 1/ray/build/CMakeFiles/ray.dir/ui/glObjects.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/lib/fltk"
+  "../libs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
